3697e1d612b5251b92e5114900a241f94134e7ef
Piotr Sociński <psocinski@gmail.com>
2017-12-27 00:09:03 +0100

    Change content at index.html and js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function getTriangleArea (a, h) {
+
+	var score;
+	
+	if ( (a > 0) && (h > 0) ) {
+    	
+    	score = a * h / 2;
+    
+    } else { 
+		
+		console.log('Nieprawidłowe dane');
+	
+	}
+
+	return score;
+}
+
+var triangle0Area = console.log(getTriangleArea(0, 0));
+var triangle1Area = console.log(getTriangleArea(10, 6));
+var triangle2Area = console.log(getTriangleArea(10, 15));
+var triangle3Area = console.log(getTriangleArea(3, 3));
